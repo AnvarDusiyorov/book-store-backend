7282d01161839c5f2be9072c1d4a2d8c9a802721
LemonLawyer <demidone@bk.ru>
2020-09-10 03:30:47 +0500

    Schema.sql updated

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,23 +1,21 @@
 create table if not exists Users(
-	username varchar(300) not null primary key,
+	email varchar(300) not null primary key,
 	password varchar(300) not null,
 	enabled boolean not null default true,
 	first_name varchar(300),
 	second_name varchar(300),
-	email varchar(300),
 	city varchar(300),
 	country varchar(300)
 );
 
 create table if not exists Authorities (
-	username varchar(300) not null,
+	user_email varchar(300) not null,
 	authority varchar(300) not null,
 	constraint fk_authorities_users
-	        foreign key(username) references Users(username) on delete cascade
+	        foreign key(user_email) references Users(email) on delete cascade
 );
 
-create unique index ix_auth_username on Authorities (username,authority);
-
+create unique index ix_auth_username on Authorities (user_email,authority);
 
 
 create table if not exists Book(
@@ -67,40 +65,40 @@ create unique index ix_book_author on Book_Author (book_id, author_id);
 create table if not exists Review(
     review_id serial primary key,
     book_id integer not null,
-    username varchar(300) not null,
+    user_email varchar(300) not null,
     description text not null,
     CONSTRAINT fk_book_id
         foreign key (book_id) references Book(book_id) on delete cascade,
     CONSTRAINT fk_username
-        foreign key (username) references Users(username) on delete cascade
+        foreign key (user_email) references Users(email) on delete cascade
 );
 
-create unique index ix_review on Review (book_id, username);
+create unique index ix_review on Review (book_id, user_email);
 
 
 
 create table if not exists Evaluate_Review(
     review_id integer not null,
-    username varchar(300) not null,
+    user_email varchar(300) not null,
     evaluation integer not null,
     CONSTRAINT fk_review_id
         foreign key (review_id) references Review(review_id) on delete cascade,
     CONSTRAINT
-        foreign key (username) references Users(username) on delete cascade
+        foreign key (user_email) references Users(email) on delete cascade
 );
 
-create unique index ix_evaluate_review on Evaluate_Review (review_id, username);
+create unique index ix_evaluate_review on Evaluate_Review (review_id, user_email);
 
 
 
 create table if not exists Rating_Book(
     book_id integer not null,
-    username varchar(300) not null,
+    user_email varchar(300) not null,
     rating integer not null,
     CONSTRAINT fk_book_id
         foreign key (book_id) references Book(book_id) on delete cascade,
     CONSTRAINT fk_username
-            foreign key (username) references Users(username) on delete cascade
+            foreign key (user_email) references Users(email) on delete cascade
 );
 
-create unique index ix_rating_book on Rating_Book (book_id, username);
+create unique index ix_rating_book on Rating_Book (book_id, user_email);
