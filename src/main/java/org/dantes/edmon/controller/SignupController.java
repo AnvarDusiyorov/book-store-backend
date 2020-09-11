4331d0401d8c0dff9cc69ba2a8cd6be1f57c19c6
LemonLawyer <demidone@bk.ru>
2020-09-11 19:59:32 +0500

    Signup added

---
+++ src/main/java/org/dantes/edmon/controller/SignupController.java
@@ -0,0 +1,45 @@
+package org.dantes.edmon.controller;
+
+import org.dantes.edmon.dto.SignupResponseDTO;
+import org.dantes.edmon.model.User;
+import org.dantes.edmon.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/signup")
+public class SignupController {
+
+    private UserService userService;
+
+    @Autowired
+    public SignupController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<SignupResponseDTO> saveUser(@RequestBody User user){
+        User userInDatabase = userService.findByEmail(user.getEmail());
+
+        SignupResponseDTO responseDTO = new SignupResponseDTO();
+        HttpStatus httpStatus = (userInDatabase == null) ? HttpStatus.CREATED : HttpStatus.CONFLICT;
+
+        if(userInDatabase == null){
+            userService.save(user);
+            responseDTO.setCreated("true");
+        }else {
+            responseDTO.setCreated("false");
+            responseDTO.setReason("such user already exist");
+        }
+
+        return new ResponseEntity<>(responseDTO, httpStatus);
+    }
+
+
+}
---
+++ src/main/java/org/dantes/edmon/dto/SignupResponseDTO.java
@@ -0,0 +1,9 @@
+package org.dantes.edmon.dto;
+
+import lombok.Data;
+
+@Data
+public class SignupResponseDTO {
+    private String created;
+    private String reason;
+}
