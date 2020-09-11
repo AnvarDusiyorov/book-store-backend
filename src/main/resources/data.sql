02585dc5fa3a6adb9dc5fd24823078d257413bc5
LemonLawyer <demidone@bk.ru>
2020-09-11 19:58:32 +0500

    Security configured and JWT support added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,15 @@
-insert into users(email,password,enabled)
-	values('admin@gmail.com','$2a$10$mfeVuYS97iE.kCcsXzetjeK.4QQefecymMRtuRJwgXHc7jUHqrse2',true);
+insert into users(email,password)
+	values('admin@gmail.com','$2a$10$mfeVuYS97iE.kCcsXzetjeK.4QQefecymMRtuRJwgXHc7jUHqrse2');
 insert into authorities(user_email,authority)
 	values('admin@gmail.com','ROLE_ADMIN');
 
-insert into users(email,password, enabled, first_name, second_name)
-    values ('Lawyer@gmail.com', '$2a$10$mfeVuYS97iE.kCcsXzetjeK.4QQefecymMRtuRJwgXHc7jUHqrse2', true, 'Ivan', 'Ivanov');
+insert into users(email,password, first_name, second_name)
+    values ('Lawyer@gmail.com', '$2a$10$mfeVuYS97iE.kCcsXzetjeK.4QQefecymMRtuRJwgXHc7jUHqrse2','Ivan', 'Ivanov');
 
 insert into authorities(user_email,authority)
 	values('Lawyer@gmail.com','ROLE_USER');
+
+
+
+
+
