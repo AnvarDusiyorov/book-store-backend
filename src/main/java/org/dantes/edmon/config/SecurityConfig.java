61ce139194910f64108af23c0064a9d9795ca679
LemonLawyer <demidone@bk.ru>
2020-09-11 19:56:32 +0500

    Security configured and JWT support added

---
+++ src/main/java/org/dantes/edmon/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package org.dantes.edmon.config;
+
+import org.dantes.edmon.jwt.JwtConfig;
+import org.dantes.edmon.jwt.JwtTokenVerifier;
+import org.dantes.edmon.jwt.JwtUsernameAndPasswordAuthenticationFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.crypto.SecretKey;
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+    private PasswordEncoder passwordEncoder;
+    private final SecretKey secretKey;
+    private final JwtConfig jwtConfig;
+
+    public SecurityConfig(DataSource dataSource, PasswordEncoder passwordEncoder,
+                          SecretKey secretKey, JwtConfig jwtConfig){
+        this.dataSource = dataSource;
+        this.passwordEncoder = passwordEncoder;
+        this.secretKey = secretKey;
+        this.jwtConfig = jwtConfig;
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select email, password, enabled from users " +
+                                "where email=?")
+                .authoritiesByUsernameQuery(
+                        "select user_email, authority from authorities " +
+                                "where user_email=?")
+                .passwordEncoder(passwordEncoder);
+    }
+
+    private final String HOME_PAGE_ENDPOINT = "/home/**";
+    private final String ADMIN_MANAGEMENT_ENDPOINT = "/management/**";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
+                    .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig), JwtUsernameAndPasswordAuthenticationFilter.class)
+                    .authorizeRequests()
+                    .antMatchers(ADMIN_MANAGEMENT_ENDPOINT)
+                        .access("hasRole('ROLE_ADMIN')")
+                    .antMatchers(HOME_PAGE_ENDPOINT)
+                        .access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
+                    .antMatchers("/**").permitAll();
+
+    }
+}
---
+++ src/main/java/org/dantes/edmon/jwt/UsernameAndPasswordAuthenticationRequest.java
@@ -0,0 +1,25 @@
+package org.dantes.edmon.jwt;
+
+public class UsernameAndPasswordAuthenticationRequest {
+    private String username; // Actually it's user's email
+    private String password;
+
+    public UsernameAndPasswordAuthenticationRequest() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
