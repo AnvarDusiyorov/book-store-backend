69bd6df87ab9f9e29c97944763d6309cef4948be
LemonLawyer <demidone@bk.ru>
2020-09-12 23:46:12 +0500

    ManagementBookDTO and PileManagementBookDTO added

---
+++ src/main/java/org/dantes/edmon/dto/managementDTO/ManagementBookDTO.java
@@ -0,0 +1,15 @@
+package org.dantes.edmon.dto.managementDTO;
+
+import lombok.Data;
+import org.dantes.edmon.model.Author;
+
+import java.util.List;
+
+@Data
+public class ManagementBookDTO {
+    private String title;
+    private Double price;
+    private List<String> genres;
+    private String imageLink;
+    private List<Author> authors;
+}
---
+++ src/main/java/org/dantes/edmon/dto/managementDTO/PileManagementBookDTO.java
@@ -0,0 +1,10 @@
+package org.dantes.edmon.dto.managementDTO;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PileManagementBookDTO {
+    private List<ManagementBookDTO> books;
+}
