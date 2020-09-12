565a910970327b4a2dd81d7f5d03d00e0c834492
LemonLawyer <demidone@bk.ru>
2020-09-12 23:44:50 +0500

    Author model added

---
+++ src/main/java/org/dantes/edmon/model/Author.java
@@ -0,0 +1,10 @@
+package org.dantes.edmon.model;
+
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+public class Author {
+    private String creativePseudonym;
+}
