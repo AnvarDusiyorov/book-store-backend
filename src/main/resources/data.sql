064ffaf2885a0d4d01e4088fe2f623644eb3f3f3
LemonLawyer <demidone@bk.ru>
2020-09-16 01:25:26 +0500

    New users added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,17 @@ insert into users(email,password, first_name, second_name)
 insert into authorities(user_email,authority)
 	values('Lawyer@gmail.com','ROLE_USER');
 
+insert into users(email,password, first_name, second_name)
+    values ('Architect@gmail.com', '$2a$10$mfeVuYS97iE.kCcsXzetjeK.4QQefecymMRtuRJwgXHc7jUHqrse2','Ted', 'Mosby');
 
+insert into authorities(user_email,authority)
+	values('Architect@gmail.com','ROLE_USER');
+
+insert into users(email,password, first_name, second_name)
+    values ('Judge@gmail.com', '$2a$10$mfeVuYS97iE.kCcsXzetjeK.4QQefecymMRtuRJwgXHc7jUHqrse2','Marshall', 'Eriksen');
+
+insert into authorities(user_email,authority)
+	values('Judge@gmail.com','ROLE_USER');
 
 
 
