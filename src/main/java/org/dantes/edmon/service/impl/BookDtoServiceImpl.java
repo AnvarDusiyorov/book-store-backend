71db169a71e770386e3ae13bf512f39e312fe49a
LemonLawyer <demidone@bk.ru>
2020-09-17 22:21:55 +0500

    Logger added

--- src/main/java/org/dantes/edmon/service/impl/BookDtoServiceImpl.java
+++ src/main/java/org/dantes/edmon/service/impl/BookDtoServiceImpl.java
@@ -1,11 +1,13 @@
 package org.dantes.edmon.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.dantes.edmon.dto.BookDTO;
 import org.dantes.edmon.repository.BookDtoRepository;
 import org.dantes.edmon.service.BookDtoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class BookDtoServiceImpl implements BookDtoService {
 
