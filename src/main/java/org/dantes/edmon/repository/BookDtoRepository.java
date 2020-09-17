9aedcea292d2841ceb9bae22ae0b10d8869f167f
LemonLawyer <demidone@bk.ru>
2020-09-17 23:59:50 +0500

    Added method getAllBookIdByGenre

--- src/main/java/org/dantes/edmon/repository/BookDtoRepository.java
+++ src/main/java/org/dantes/edmon/repository/BookDtoRepository.java
@@ -2,7 +2,10 @@ package org.dantes.edmon.repository;
 
 import org.dantes.edmon.dto.BookDTO;
 
+import java.util.List;
+
 public interface BookDtoRepository {
     BookDTO findBookByBookID(Integer bookID);
     Integer getRatingGivenByUser(String userEmail);
+    List<Integer> getAllBookIdByGenre(String genreName);
 }
