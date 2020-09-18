5e1e73c82b12a9fbd67050083252375f0dc03cc8
LemonLawyer <demidone@bk.ru>
2020-09-18 23:41:51 +0500

    Added new methods

--- src/main/java/org/dantes/edmon/repository/BookDtoRepository.java
+++ src/main/java/org/dantes/edmon/repository/BookDtoRepository.java
@@ -1,6 +1,8 @@
 package org.dantes.edmon.repository;
 
 import org.dantes.edmon.dto.BookDTO;
+import org.dantes.edmon.dto.ShortViewBookDTO;
+import org.dantes.edmon.dto.search.SearchRequestDTO;
 
 import java.util.List;
 
@@ -8,4 +10,7 @@ public interface BookDtoRepository {
     BookDTO findBookByBookID(Integer bookID);
     Integer getRatingGivenByUser(String userEmail);
     List<Integer> getAllBookIdByGenre(String genreName);
+    ShortViewBookDTO findShortViewBookByBookID(Integer bookID);
+    List<String> getAllGenres();
+    List<ShortViewBookDTO> getAllBooksBySearchRequestDTO(SearchRequestDTO searchRequestDTO);
 }
--- src/main/java/org/dantes/edmon/service/BookDtoService.java
+++ src/main/java/org/dantes/edmon/service/BookDtoService.java
@@ -1,8 +1,19 @@
 package org.dantes.edmon.service;
 
 import org.dantes.edmon.dto.BookDTO;
+import org.dantes.edmon.dto.PileOfGenres;
+import org.dantes.edmon.dto.ShortViewBookDTO;
+import org.dantes.edmon.dto.search.PileBookDTO;
+import org.dantes.edmon.dto.search.SearchRequestDTO;
+
+import java.util.List;
 
 public interface BookDtoService {
     BookDTO findBookByBookID(Integer bookID);
     Integer getRatingGivenByUser(String userEmail);
+    List<Integer> getAllBookIdByGenre(String genreName);
+    ShortViewBookDTO findShortViewBookByBookID(Integer bookID);
+    PileOfGenres getAllGenres();
+    List<ShortViewBookDTO> getAllShortViewBooksByListOfBookID(List<Integer> bookIdList);
+    PileBookDTO getAllShortViewBooksBySearchRequestDTO(SearchRequestDTO searchRequestDTO);
 }
