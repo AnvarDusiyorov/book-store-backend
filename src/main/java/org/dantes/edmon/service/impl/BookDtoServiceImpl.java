c483bb09bb59c4c82e64622dd158622ebf0fb22b
LemonLawyer <demidone@bk.ru>
2020-09-18 23:43:02 +0500

    Added implementations of new methods

--- src/main/java/org/dantes/edmon/service/impl/BookDtoServiceImpl.java
+++ src/main/java/org/dantes/edmon/service/impl/BookDtoServiceImpl.java
@@ -2,10 +2,17 @@ package org.dantes.edmon.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.dantes.edmon.dto.BookDTO;
+import org.dantes.edmon.dto.PileOfGenres;
+import org.dantes.edmon.dto.ShortViewBookDTO;
+import org.dantes.edmon.dto.search.PileBookDTO;
+import org.dantes.edmon.dto.search.SearchRequestDTO;
 import org.dantes.edmon.repository.BookDtoRepository;
 import org.dantes.edmon.service.BookDtoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -41,6 +48,61 @@ public class BookDtoServiceImpl implements BookDtoService {
         }catch (Exception e){ }
 
         return userRating;
+    }
+
+    @Override
+    public List<Integer> getAllBookIdByGenre(String genreName) {
+
+        List<Integer> bookIdList = null;
+
+        try {
+            bookIdList = bookDtoRepository.getAllBookIdByGenre(genreName);
+            if(bookIdList.isEmpty()){
+                bookIdList = null;
+            }
+        }catch (Exception e){
+        }
+
+        return bookIdList;
+    }
+
+    @Override
+    public ShortViewBookDTO findShortViewBookByBookID(Integer bookID) {
+        ShortViewBookDTO shortViewBookDTO = null;
+
+        try {
+            shortViewBookDTO = bookDtoRepository.findShortViewBookByBookID(bookID);
+        }catch (Exception e){
+
+        }
+
+        return shortViewBookDTO;
+    }
+
+    @Override
+    public PileOfGenres getAllGenres() {
+        PileOfGenres pileOfGenres = new PileOfGenres();
+        pileOfGenres.setGenres(bookDtoRepository.getAllGenres());
+        return pileOfGenres;
+    }
+
+    @Override
+    public List<ShortViewBookDTO> getAllShortViewBooksByListOfBookID(List<Integer> bookIdList) {
+        List<ShortViewBookDTO> books = new ArrayList<>();
+
+        for (Integer bookID : bookIdList){
+            books.add(findShortViewBookByBookID(bookID));
+        }
+
+        return books;
+    }
+
+
+    @Override
+    public PileBookDTO getAllShortViewBooksBySearchRequestDTO(SearchRequestDTO searchRequestDTO) {
+        PileBookDTO pileBookDTO = new PileBookDTO();
+        pileBookDTO.setBooks(bookDtoRepository.getAllBooksBySearchRequestDTO(searchRequestDTO));
+        return pileBookDTO;
     }
 
 }
